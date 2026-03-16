ea0468fa2e256b5ba13bd1d1206685a3de827039
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-03-17 01:12:41 +0300

    addendum test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,3 @@
-
 import pytest
 from src.main import Product, Category
 
@@ -6,19 +5,18 @@ from src.main import Product, Category
 @pytest.fixture
 def product_1():
     return Product(
-        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера",
-        180000.0, 5
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
     )
 
 
 @pytest.fixture
-def category_1():
+def category_1(product_1):
     return Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации,\
         но и получения дополнительных функций для удобства жизни",
-        ["product1", "product2", "product3"])
-
+        [product_1]
+    )
 
 
 def test_init(product_1):
@@ -32,7 +30,16 @@ def test_init_category(category_1):
     assert category_1.name == "Смартфоны"
     assert category_1.description == ("Смартфоны, как средство не только коммуникации,\
         но и получения дополнительных функций для удобства жизни")
-    assert category_1.products == ["product1", "product2", "product3"]
-    assert len(category_1.products) == 3
+    assert category_1.products == "Samsung Galaxy S23 Ultra, 180000.0 руб. 5 шт.\n"
+    assert len(category_1.products) == 46
     assert category_1.category_count == 1
-    assert category_1.product_count == 3
+    assert category_1.product_count == 1
+
+def test_category_products_property(category_1):
+    assert category_1.products == "Samsung Galaxy S23 Ultra, 180000.0 руб. 5 шт.\n"
+
+def test_category_add_product(category_1):
+    product_4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    category_1.add_product(product_4)
+    assert category_1.products == ('Samsung Galaxy S23 Ultra, 180000.0 руб. 5 шт.\n'
+ '55" QLED 4K, 123000.0 руб. 7 шт.\n')
