b81f84b76d95236a0af9dd07adcc5f9b34b362cf
Margo_Borodinowa <bmargaritaw@yandex.ru>
2026-03-20 01:21:43 +0300

    correction test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,6 +21,13 @@ def test_init(product_1):
     assert product_1.quantity == 5
     assert product_1.price == 180000.0
 
+def test_str_product(product_1):
+    assert str(product_1) == 'Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.'
+
+def test_add_product(product_1):
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    assert(product_1 + product2) == 2580000.0
+
 
 def test_init_category(category_1):
     assert category_1.name == "Смартфоны"
@@ -31,6 +38,8 @@ def test_init_category(category_1):
     assert category_1.category_count == 1
     assert category_1.product_count == 1
 
+def test_str_category(category_1):
+    assert str(category_1) == 'Смартфоны, количество продуктов: 5'
 
 def test_category_products_property(category_1):
     assert category_1.products == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\n"
